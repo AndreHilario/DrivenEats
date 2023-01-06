52dbf9d37a6b5addef20f83a81e4fb355cc80a2c
AndreHilario <andre18hilario@gmail.com>
2023-01-06 09:32:30 -0300

    Versão com função de mudar o botão de pedido

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let prato;
+let bebida;
+let sobremesa;
+
 function selecionarPrato(botaoClicado) {
 
     const pratoAnterior = document.querySelector('.pratos .selecionado');
@@ -9,7 +13,11 @@ function selecionarPrato(botaoClicado) {
 
     botaoClicado.classList.add('selecionado');
 
+    prato = botaoClicado.classList.contains('selecionado');
+    console.log(prato);
 }
+
+
 function selecionarBebida(botaoClicado2) {
 
     const bebidaAnterior = document.querySelector('.bebidas .selecionado');
@@ -21,7 +29,11 @@ function selecionarBebida(botaoClicado2) {
 
     botaoClicado2.classList.add('selecionado');
 
+    bebida = botaoClicado2.classList.contains('selecionado');
+    console.log(bebida);
 }
+    
+
 function selecionarSobremesa(botaoClicado3) {
 
     const sobremesaAnterior = document.querySelector('.sobremesas .selecionado');
@@ -33,5 +45,19 @@ function selecionarSobremesa(botaoClicado3) {
 
     botaoClicado3.classList.add('selecionado');
 
+    sobremesa = botaoClicado3.classList.contains('selecionado');
+    console.log(sobremesa);
 }
+
+function botaoFinalizar() {
+
+    if(prato === true && bebida === true && sobremesa === true) {
+        
+        const botao = document.querySelector('.botao-finalizar');
+        botao.classList.add('botao-ativo');
+        botao.innerHTML = "Fechar pedido";
+    }
+}
+
+
 
