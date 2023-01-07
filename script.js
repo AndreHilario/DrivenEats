314155bb248451de4533f697dcb5e9fe79d64949
AndreHilario <andre18hilario@gmail.com>
2023-01-07 09:55:40 -0300

    Versão com texto do pedido

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 let prato;
 let bebida;
 let sobremesa;
+let nomePrato;
+let nomeBebida;
+let nomeSobremesa;
+let valorPrato;
+let valorBebida;
+let valorSobremesa;
 
 function selecionarPrato(botaoClicado) {
 
@@ -12,9 +18,15 @@ function selecionarPrato(botaoClicado) {
     }
 
     botaoClicado.classList.add('selecionado');
-
+    
     prato = botaoClicado.classList.contains('selecionado');
     console.log(prato);
+
+    if(prato === true){
+        botaoClicado = document.querySelector('.pratos .nome');
+        nomePrato = botaoClicado.innerHTML;
+    }
+
 }
 
 
@@ -26,14 +38,19 @@ function selecionarBebida(botaoClicado2) {
 
         bebidaAnterior.classList.remove('selecionado');
     }
+    
 
     botaoClicado2.classList.add('selecionado');
 
     bebida = botaoClicado2.classList.contains('selecionado');
     console.log(bebida);
+
+    if(bebida === true){
+        botaoClicado2 = document.querySelector('.bebidas .nome');
+        nomeBebida = botaoClicado2.innerHTML;
+    }
 }
     
-
 function selecionarSobremesa(botaoClicado3) {
 
     const sobremesaAnterior = document.querySelector('.sobremesas .selecionado');
@@ -47,6 +64,11 @@ function selecionarSobremesa(botaoClicado3) {
 
     sobremesa = botaoClicado3.classList.contains('selecionado');
     console.log(sobremesa);
+
+    if(sobremesa === true){
+        botaoClicado3 = document.querySelector('.sobremesas .nome');
+        nomeSobremesa = botaoClicado3.innerHTML;
+    }
 }
 
 function botaoFinalizar() {
@@ -57,6 +79,19 @@ function botaoFinalizar() {
         botao.classList.add('botao-ativo');
         botao.innerHTML = "Fechar pedido";
     }
+    const habilitarBotao = document.querySelector('.botao-finalizar');
+    habilitarBotao.removeAttribute("disabled");
+}
+
+function fazerPedido() {
+    let mensagem;
+
+    mensagem = `Olá, gostaria de fazer o pedido:
+              - Prato: ${nomePrato}
+              - Bebida: ${nomeBebida}
+              - Sobremesa: ${nomeSobremesa}
+              Total: R$ ${valorPrato + valorBebida + valorSobremesa}`
+    alert(mensagem);
 }
 
 
